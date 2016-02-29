e0d8bda9c744d97865189da8f2603b2ddb007605
ipcrm <mcadorette@puppetlabs.com>
2016-02-29 16:47:46 -0500

    update param location, round 1

--- manifests/active.pp
+++ manifests/active.pp
@@ -1,27 +1,7 @@
 class pe_failover::active (
   String $passive_master,
-  Array $exclude_certs          = [],
-  String $rsync_user            = $pe_failover::params::rsync_user,
-  String $rsync_user_home       = $pe_failover::params::rsync_user_home,
-  String $rsync_user_ssh_id     = $pe_failover::params::rsync_user_ssh_id,
-  String $rsync_ssl_dir         = $pe_failover::params::rsync_ssl_dir,
-  String $rsync_command         = $pe_failover::params::rsync_command,
-  String $incron_ssl_condition  = $pe_failover::params::incron_ssl_condition,
-  String $script_directory      = $pe_failover::params::script_directory,
-  String $pe_failover_directory = $pe_failover::params::pe_failover_directory,
-  Array $pe_bkup_dbs            = $pe_failover::params::pe_bkup_dbs,
-  String $minute                = $pe_failover::params::minute,
-  String $hour                  = $pe_failover::params::hour,
-  String $monthday              = $pe_failover::params::monthday,
-  String $sync_minute           = $pe_failover::params::sync_minute,
-  String $sync_hour             = $pe_failover::params::sync_hour,
-  String $sync_monthday         = $pe_failover::params::sync_monthday,
-  String $dump_path             = $pe_failover::params::dump_path,
-  String $nc_dump_path          = $pe_failover::params::nc_dump_path,
-  String $cert_dump_path        = $pe_failover::params::cert_dump_path,
 ) inherits pe_failover::params {
 
-  require ::pe_failover
   include ::pe_failover::active::ssh
   include ::pe_failover::active::db
   include ::pe_failover::active::scripts
--- manifests/passive.pp
+++ manifests/passive.pp
@@ -1,20 +1,7 @@
 class pe_failover::passive (
   String $auth_key,
-  String $rsync_user                   = $pe_failover::params::rsync_user,
-  Hash $pe_users                       = $pe_failover::params::pe_users,
-  String $pe_failover_directory        = $pe_failover::params::pe_failover_directory,
-  String $script_directory             = $pe_failover::params::script_directory,
-  String $dump_path                    = $pe_failover::params::dump_path,
-  String $nc_dump_path                 = $pe_failover::params::nc_dump_path,
-  String $restore_nc_minute            = $pe_failover::params::restore_nc_minute,
-  String $restore_db_minute            = $pe_failover::params::restore_db_minute,
-  String $restore_hour                 = $pe_failover::params::restore_hour,
-  String $restore_monthday             = $pe_failover::params::restore_monthday,
-  String $incron_passive_ssl_condition = $pe_failover::params::incron_passive_ssl_condition,
-  Array $pe_bkup_dbs                   = $pe_failover::params::pe_bkup_dbs,
 ) inherits pe_failover::params{
 
-  require ::pe_failover
   include ::pe_failover::passive::paths
   include ::pe_failover::passive::users
   include ::pe_failover::passive::ssh
