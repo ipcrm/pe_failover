015fecc94d4e306682d5ba06e63603a793f05a83
ipcrm <mcadorette@puppetlabs.com>
2016-03-01 13:40:42 -0500

    added code for classification

---
+++ manifests/active/classification.pp
@@ -0,0 +1,37 @@
+class pe_failover::active::classification {
+
+  package{'puppetclassify':
+    ensure   => present,
+    provider => 'puppet_gem',
+  }
+
+  node_group { 'pe_failover':
+    ensure               => 'present',
+    environment          => 'production',
+    override_environment => false,
+    parent               => 'All Nodes',
+    classes              => {'pe_failover' => {}},
+    rule                 => ['and', ['~', ['fact', 'pe_failover_mode'], '(active|passive)']],
+    require              => Package['puppetclassify'],
+  }
+
+  node_group { 'pe_failover-active':
+    ensure               => 'present',
+    classes              => {'pe_failover::active' => {}},
+    environment          => 'production',
+    override_environment => false,
+    parent               => 'All Nodes',
+    rule                 => ['and', ['~', ['fact', 'pe_failover_mode'], 'active']],
+    require              => Node_group['pe_failover'],
+  }
+
+  node_group { 'pe_failover-passive':
+    ensure               => 'present',
+    classes              => {'pe_failover::passive' => {}},
+    environment          => 'production',
+    override_environment => false,
+    parent               => 'All Nodes',
+    rule                 => ['and', ['~', ['fact', 'pe_failover_mode'], 'passive']],
+    require              => Node_group['pe_failover'],
+  }
+}
